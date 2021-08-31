0b4f220bddf25600489f9f333be2fd64e14ce1da
KOUSHA_ASLANI <kousha.aslani@gmail.com>
2021-08-31 17:40:38 +1000

    played around with some lesson files, week4 challenge in main file still

--- Lesson 1 - Basics/1_read.py
+++ Lesson 1 - Basics/1_read.py
@@ -1,27 +1,29 @@
 import cv2
 import numpy
 
-#reading images
-img = cv2.imread('../Photos/cat.jpg')
-cv2.imshow('Cat', img)
+# #reading images
+# img = cv2.imread('../Photos/cat.jpg')
+# cv2.imshow('Cat', img)
+
 
 # reading videos
 
-#capture = cv2.VideoCapture('..\Videos\dog.mp4')
-#VideoCapture(0) = Webcam input
+capture = cv2.VideoCapture('../Videos/dog.mp4')
+# VideoCapture(0) = Webcam input
 
-#capture=cv2.VideoCapture(0)
+# capture=cv2.VideoCapture(0)
 
-#while True:
-    #isTrue,frame=capture.read()
+while True:
+    isTrue,frame=capture.read()
     #show frame
-    #cv2.imshow('',frame)
+    cv2.imshow('',frame)
 
     #if the d key is pressed, kill screen
-    #if cv2.waitKey(20) & 0xFF==ord('d'):
-        #break
+    if cv2.waitKey(20) & 0xFF==ord('d'):
+        break
 
-#capture.release()
-#cv2.destroyAllWindows()
+capture.release()
+cv2.destroyAllWindows()
+
 
 cv2.waitKey(0)
--- Lesson 1 - Basics/2_resizeRescale.py
+++ Lesson 1 - Basics/2_resizeRescale.py
@@ -2,8 +2,8 @@
 
 import cv2
 
-img = cv2.imread('../Photos/cat_large.jpg')
-cv2.imshow('Cat',img)
+#img = cv2.imread('Photos\cat_large.jpg')
+#cv2.imshow('Cat',img)
 
 def rescale_frame(frame,scale=0.75):
     #works for images, video and live video
@@ -13,36 +13,39 @@ def rescale_frame(frame,scale=0.75):
 
     return cv2.resize(frame,dimensions,interpolation =cv2.INTER_AREA)
 
+#cv2.waitKey(0)
 
-SmallCat = rescale_frame(img,0.5)
-cv2.imshow('SMallerCat', SmallCat)
-
-cv2.waitKey(0)
-
-def change_res(width,height):
-    #only works for live video
-    capture.set(3,width) #3 for width
-    capture.set(4,height) #4 for height
+### FUNCTION POINTLESS ON MAC AS WEBCAM HAS FIXED RESOLUTION ### 
+# def change_res(width,height):
+#     #only works for live video
+#     capture.set(3,width) #3 for width
+#     capture.set(4,height) #4 for height
 
 #reading videos
 
-# capture = cv2.VideoCapture('../Videos/dog.mp4')
-#VideoCapture(0) = Webcam input
-#capture=cv2.VideoCapture(0)
+# capture = cv2.VideoCapture('../Videos/kitten.mp4')
+# VideoCapture(0) = Webcam input
+capture=cv2.VideoCapture(0)
+# change_res(160,120)
 
-# while True:
-#     isTrue,frame=capture.read()
+while True:
+    isTrue, frame=capture.read()
+
+    # cv2.imshow("video with set", frame)
+    # cv2.imshow("Video Resized", frame_resized)
     
-#     #recsale video frame
-#     frame_resized=rescale_frame(frame)
+    #recsale video frame
+    frame_resized = rescale_frame(frame, scale = 0.5)
+
+    cv2.imshow('video with set',frame)
+    cv2.imshow('Resized',frame_resized)
+
+    #20 is the delay
+    if cv2.waitKey(20) & 0xFF==ord('d'):
+        break
 
 
-#     cv2.imshow('',frame)
-#     cv2.imshow('Resized',frame_resized)
 
-#     #20 is the delay
-#     if cv2.waitKey(20) & 0xFF==ord('d'):
-#         break
 
-# capture.release()
-# cv2.destroyAllWindows()
+capture.release()
+cv2.destroyAllWindows()
